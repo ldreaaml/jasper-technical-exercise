64e5b87682487f7656dd26077b0252bdb2bf0aec
Dream Jitsawatpaiboon <dreamza43@gmail.com>
2023-01-23 13:16:48 +1300

    set up project with tailwind

---
+++ jasper/tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{jsx,js}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        blue: "#0f46de",
+        darkBlue: "#14246c",
+        error: "#ff1344",
+      },
+    },
+  },
+  plugins: [],
+};
