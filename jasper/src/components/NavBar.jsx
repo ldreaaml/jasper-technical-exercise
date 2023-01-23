01815c0f6bc764eda0a5eea7c528316ba714316b
Dream Jitsawatpaiboon <dreamza43@gmail.com>
2023-01-23 19:27:30 +1300

    create popup modal

--- jasper/src/components/NavBar.jsx
+++ jasper/src/components/NavBar.jsx
@@ -9,10 +9,29 @@ const NavBar = () => {
   return (
     <>
       <div className="flex justify-between items-center bg-blue p-3 px-12">
-        <p className="text-white text-4xl">Jasper</p>
-        <button className="font-medium bg-transparent text-white border border-white py-2 px-4 rounded hover:bg-white hover:text-blue">
-          Create Account
-        </button>
+        <p className="text-white text-4xl font-medium">Jasper</p>
+        {isPopupVisible ? (
+          <button className="font-medium bg-transparent text-white border border-white py-2 px-4 rounded hover:bg-white hover:text-blue">
+            Create Account
+          </button>
+        ) : (
+          <>
+            <div className="text-white space-x-12 ">
+              <a href="#" className="opacity-80 hover:opacity-100">
+                Opportunities
+              </a>
+              <a href="#" className="opacity-80 hover:opacity-100">
+                My Portfolio
+              </a>
+              <a href="#" className="opacity-80 hover:opacity-100">
+                Notificatoins
+              </a>
+            </div>
+            <button className="font-medium bg-transparent text-white border border-white py-2 px-4 rounded hover:bg-white hover:text-blue">
+              Create Account
+            </button>
+          </>
+        )}
       </div>
     </>
   );
--- jasper/src/components/PopUp.jsx
+++ jasper/src/components/PopUp.jsx
@@ -3,14 +3,44 @@ import AccountDetailForm from "./AccountDetailForm";
 import CreateAccountForm from "./CreateAccountForm";
 
 const PopUp = () => {
+  const [isPopupVisible, setIsPopupVisible] = useState(true);
+  const [accountFormSuccess, setAccountFormSucess] = useState(false);
+  const closePopup = () => {
+    setIsPopupVisible(false);
+  };
+
   return (
     <>
-      <div className="bg-slate-300 p-3 flex justify-center">
-        <div className="bg-white max-w-lg p-6 rounded-md">
-          {/* <CreateAccountForm /> */}
-          <AccountDetailForm />
+      {isPopupVisible ? (
+        <div
+          className="relative z-10"
+          aria-labelledby="modal-title"
+          role="dialog"
+          aria-modal="true"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+
+          <div className="fixed inset-0 z-10 overflow-y-auto">
+            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <button
+                  className="absolute right-2 top-0 p-3 font-medium text-xl text-gray-400 cursor-pointer"
+                  onClick={closePopup}
+                >
+                  x
+                </button>
+                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                  {accountFormSuccess ? (
+                    <CreateAccountForm />
+                  ) : (
+                    <AccountDetailForm />
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+      ) : null}
     </>
   );
 };
